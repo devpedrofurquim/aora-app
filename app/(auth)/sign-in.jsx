b761a8fa16c454d524dd0ac04d0f49265ee13fe0
devpedrofurquim <pedrofurquim.dev@gmail.com>
2024-06-23 17:20:08 -0300

    feat: Add Form component to sign-in screen

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,11 +1,24 @@
-import { View, Text } from 'react-native'
+import { View, Image, Text, ScrollView } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { images } from '../../constants'
+import Form from '../../components/Form'
 
 const SignIn = () => {
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView>
+        <View className="w-full justify-center h-full px-4 my-6">
+           <Image
+            source={images.logo}
+            resizeMode='contain'
+            className="w-[115px] h-[35px]"
+           />
+           <Text className="text-2xl text-white text-semibold mt-10 font-psemibold">Log in to Aora</Text>
+           <Form/>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
---
+++ components/Form.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Form = () => {
+  return (
+    <View className="space-y-2">
+      <Text className="text-base text-gray-100 font-pmedium">Form</Text>
+    </View>
+  )
+}
+
+export default Form
