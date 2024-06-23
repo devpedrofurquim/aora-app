f297f61790cb7af29da7a96e0a1f303b19cfa02b
devpedrofurquim <pedrofurquim.dev@gmail.com>
2024-06-23 00:16:42 -0300

    feat: Load fonts inside layout

--- app/index.jsx
+++ app/index.jsx
@@ -5,7 +5,7 @@ import { Link } from 'expo-router';
 export default function App() {
   return (
    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-2xl">AoOra</Text>
+      <Text className="text-2xl font-psemibold">AoOra App</Text>
       <StatusBar style="auto" />
       <Link href="/profile" style={{color: 'blue'}}>Go to profile</Link>
     </View>
