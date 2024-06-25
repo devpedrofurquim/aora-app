cba1f4a7012a6dcd2e9b69455f3af9d9eb29ebe4
devpedrofurquim <pedrofurquim.dev@gmail.com>
2024-06-24 21:23:49 -0300

    feat: finish sign-in screen

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -3,6 +3,8 @@ import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { images } from '../../constants'
 import Form from '../../components/Form'
+import Button from '../../components/Button'
+import { Link } from 'expo-router'
 
 const SignIn = () => {
   const [form, setForm] = useState({
@@ -10,10 +12,12 @@ const SignIn = () => {
     password: ''
   })
 
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView>
-        <View className="w-full justify-center h-full px-4 my-6">
+        <View className="w-full justify-center min-h-[85vh] px-4 my-6">
            <Image
             source={images.logo}
             resizeMode='contain'
@@ -27,6 +31,7 @@ const SignIn = () => {
             otherStyles="mt-7"
             keyboardType="email-address"
            />
+
            <Form
             title="Password"
             value={form.password}
@@ -34,6 +39,17 @@ const SignIn = () => {
             otherStyles="mt-7"
             keyboardType="password"
            />
+
+          <Button
+            children='Continue with Email'
+            handlePress={() => {}}
+            containerStyles='mt-7'
+            isLoading={isSubmitting}
+          />
+          <View className='justify-center pt-5 flex-row gap-2'>
+            <Text className='text-lg text-gray-100 font-pregular'>Don't have an account?</Text>
+            <Link href="/sign-up" className='text-lg font-psemibold text-secondary'>Sign Up</Link>
+          </View>
         </View>
       </ScrollView>
     </SafeAreaView>
